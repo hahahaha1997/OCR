3b1e1928a41648787f9a8ef3b286febf950d3a80
damndamndamn <794196368@qq.com>
2018-08-08 01:34:31 +0800

    final commit

---
+++ README.md
@@ -0,0 +1,2 @@
+introduction blog:https://www.cnblogs.com/DawnSwallow/p/9440516.html
+1~10.png can be correctly recognized in the root dictory.
--- ocr.py
+++ ocr.py
@@ -2,6 +2,33 @@ from os import listdir
 import numpy
 import operator
 import cv2
+
+def imProcess(imagePath):
+    testDigits = listdir(imagePath)
+    for i in range(len(testDigits)):
+        imageName = testDigits[i]#图像命名格式为N_M.png，NM含义见4）生成训练样本
+        #imageClass = int((imageName.split('.')[0]).split('_')[0])#这个图像的数字是多少
+        image = cv2.imread(imageName,cv2.IMREAD_GRAYSCALE)
+        image = cv2.resize(image, (32, 32), interpolation=cv2.INTER_AREA)
+        ret, image = cv2.threshold(image, 150, 255, cv2.THRESH_BINARY)
+        with open(r'F:\Users\yang\PycharmProjects\OCR_KNN\testDigits\\'+imageName.split('.')[0]+'.txt','w+') as file:
+            for i in range(32):
+                for j in range(32):
+                    if image[i][j] == 255:
+                        file.write('0')
+                    else:
+                        file.write('1')
+                file.writelines('\n')
+
+def img2vector(filename):
+    returnvec = numpy.zeros((1,1024))
+    file = open(filename)
+    for i in range(32):
+        line = file.readline()
+        for j in range(32):
+            returnvec[0,32*i+j] = int(line[j])
+    return returnvec
+
 def handWritingClassifyTest():
     labels=[]
     trainingFile = listdir(r'F:\Users\yang\PycharmProjects\OCR_KNN\trainingDigits')
@@ -25,15 +52,6 @@ def handWritingClassifyTest():
         if(result!=classnum_test):error+=1.0
     print("准确率：%f"%(1.0-(error/float(testnum))))
 
-def img2vector(filename):
-    returnvec = numpy.zeros((1,1024))
-    file = open(filename)
-    for i in range(32):
-        line = file.readline()
-        for j in range(32):
-            returnvec[0,32*i+j] = int(line[j])
-    return returnvec
-
 def classify(inX,dataSet,labels,k):
     size = dataSet.shape[0]
     distance = (((numpy.tile(inX,(size,1))-dataSet)**2).sum(axis=1))**0.5
@@ -51,23 +69,23 @@ def dict2list(dic:dict):#将字典转换为list类型
     lst=[(key, value)for key,value in zip(keys,values)]
     return lst
 
-def imProcess(image):
-    image = cv2.resize(image, (32, 32), interpolation=cv2.INTER_AREA)
-    ret, image = cv2.threshold(image, 150, 255, cv2.THRESH_BINARY)
-
-    with open(r'F:\Users\yang\PycharmProjects\OCR_KNN\testDigits\6_0.txt','w+') as file:
-        for i in range(32):
-            for j in range(32):
-                if image[i][j] == 255:
-                    file.write('0')
-                else:
-                    file.write('1')
-            file.writelines('\n')
+# def imProcess(image):
+#     image = cv2.resize(image, (32, 32), interpolation=cv2.INTER_AREA)
+#     ret, image = cv2.threshold(image, 150, 255, cv2.THRESH_BINARY)
+#     cv2.imshow('result',image)
+#     cv2.waitKey(0)
+#     with open(r'F:\Users\yang\PycharmProjects\OCR_KNN\testDigits\6_0.txt','w+') as file:
+#         for i in range(32):
+#             for j in range(32):
+#                 if image[i][j] == 255:
+#                     file.write('0')
+#                 else:
+#                     file.write('1')
+#             file.writelines('\n')
 
 
 
 # iamge = cv2.imread(r'C:\Users\yang\Desktop\6.png',cv2.IMREAD_GRAYSCALE)
 # image = imProcess(iamge)
-
-# imProcess(image0_2)
+imProcess(r'F:\Users\yang\PycharmProjects\OCR_KNN')
 handWritingClassifyTest()
